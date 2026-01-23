74daf7b4f14bad5eaa7c8927ac0eb75090ccffa5
Catsss3 <89249635+Catsss3@users.noreply.github.com>
2026-01-23 17:04:45 +0700

    💄 Blondie: No-slash fix 💅

--- worker.py
+++ worker.py
@@ -1,17 +1,8 @@
 
-def run():
-    with open("lists/active_endpoints.txt", "r") as f: sni = f.readline().strip()
-    with open("input/fresh_raw_links.txt", "r") as f: links = [l.strip() for l in f if l.strip()]
-    
-    output = []
+with open("lists/active_endpoints.txt", "r") as f: sni = f.readline().strip()
+with open("input/fresh_raw_links.txt", "r") as f: links = [l.strip() for l in f if l.strip()]
+with open("subscription.txt", "w") as f:
+    # Пишем через print, он на Гитхабе ВСЕГДА делает нормальный перенос
     for i, l in enumerate(links):
         base = l.split('?')[0]
-        line = f"{base}?encryption=none&security=tls&sni={sni}&type=ws#Blondie_Vip_{i+1}"
-        output.append(line)
-    
-    # Соединяем строки настоящим символом переноса
-    with open("subscription.txt", "w") as f:
-        f.write('\n'.join(output))
-
-if __name__ == '__main__':
-    run()
+        print(f"{base}?encryption=none&security=tls&sni={sni}&type=ws#Blondie_Vip_{i+1}", file=f)
