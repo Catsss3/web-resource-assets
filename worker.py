3e7f655e4ac9428ee8dd68af321199141ec149d0
Catsss3 <89249635+Catsss3@users.noreply.github.com>
2026-01-23 17:22:38 +0700

    🚀 Blondie: Worker to root

--- worker.py
+++ worker.py
@@ -1,8 +1,18 @@
 
-with open("lists/active_endpoints.txt", "r") as f: sni = f.readline().strip()
-with open("input/fresh_raw_links.txt", "r") as f: links = [l.strip() for l in f if l.strip()]
-with open("subscription.txt", "w") as f:
-    # Пишем через print, он на Гитхабе ВСЕГДА делает нормальный перенос
-    for i, l in enumerate(links):
-        base = l.split('?')[0]
-        print(f"{base}?encryption=none&security=tls&sni={sni}&type=ws#Blondie_Vip_{i+1}", file=f)
+import os
+def run():
+    try:
+        # Пути теперь фиксированные
+        with open("lists/active_endpoints.txt", "r") as f: sni = f.readline().strip()
+        with open("input/fresh_raw_links.txt", "r") as f: links = [l.strip() for l in f if l.strip()]
+        
+        with open("subscription.txt", "w") as f:
+            for i, l in enumerate(links):
+                base = l.split('?')[0]
+                f.write(f"{base}?encryption=none&security=tls&sni={sni}&type=ws#Blondie_Vip_{i+1}\n")
+        print("✅ Success!")
+    except Exception as e:
+        print(f"❌ Error: {e}")
+
+if __name__ == '__main__':
+    run()
