04136ca239257ced371d1058c1e15a57da56a6ba
Catsss3 <89249635+Catsss3@users.noreply.github.com>
2026-01-23 15:26:08 +0700

    💄 Blondie: TOTAL FIX NO MORE BACKSLASH 🎀

--- worker.py
+++ worker.py
@@ -4,5 +4,5 @@ with open("input/fresh_raw_links.txt", "r") as f: links = f.readlines()
 with open("subscription.txt", "w") as f:
     for i, l in enumerate(links):
         base = l.strip().split('?')[0]
-        # Добавляем \n в конце каждой строки!
-        f.write(f"{base}?encryption=none&security=tls&sni={sni}&type=ws#Blondie_Vip_{i+1}\n")
+        # Используем обычный print, он САМ добавит правильный перенос строки
+        print(f"{base}?encryption=none&security=tls&sni={sni}&type=ws#Blondie_Vip_{i+1}", file=f)
